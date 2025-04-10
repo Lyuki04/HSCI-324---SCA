7a638144c5ace26ca4174e70dac6722170e78ce6
Lyuki04 <luka.d.yuki@gmail.com>
2025-04-10 04:39:49 +0000

    Made a change

--- pages/8_Questions.py
+++ pages/8_Questions.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.title("Questions")
+
+st.header("Fill In The Blank")
+
+st.subheader("The genetic condition causing most dominant forms of SCA involves toxic expansion of this repeating sequence.")
+with st.expander("Answer"):
+    st.markdown("""
+    CAG 
+    """)
