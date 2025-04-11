6c65b7bda04fafacd75627aaa5b6cfde87fc7cc0
Lyuki04 <luka.d.yuki@gmail.com>
2025-04-11 05:28:08 +0000

    Made a change

--- pages/3_Genetic Drift.py
+++ pages/3_Genetic Drift.py
@@ -13,8 +13,6 @@ lottie_coding = "https://lottie.host/d921db5e-1ee5-41b7-a07d-9c50be5406f7/aMx9k0
 
 img_gene = Image.open("Images/Genetic_Drift.png")
 
-st.image(img_gene)
-
 left_column, right_column = st.columns((2,3))
 with left_column:
     st.title("Genetic Drift")
@@ -46,7 +44,7 @@ with st.expander("What is Genetic Drift?"):
 - More pronounced in **small populations**.<sup>1,2</sup>
 - Over time, some genes may become **more or less common**, or even disappear entirely.<sup>1,2</sup>
     """, unsafe_allow_html=True)
-    st.image("img_gene")
+    st.image(img_gene)
     st.markdown("_Source: https://www.khanacademy.org/science/ap-biology/natural-selection/population-genetics/a/genetic-drift-founder-bottleneck_")
 
 with st.expander("Common Types of Genetic Drift"):
